abf18dc8be920d2a96682af0769a62e98fadcdef
arthur dumont <dumarthda@gmail.com>
2024-10-03 16:29:42 +0200

    git carte

---
+++ carte/agents.js
@@ -0,0 +1,28 @@
+const conseilsData = {
+    Ash: "Ash est un perso qui est très rapide moi personnellement je joue la R4C très bon agent pour débutez le jeux.",
+    Thermite: "perso .",
+    IQ: "Utilisez votre gadget pour repérer les pièges avant d'entrer dans une pièce.",
+    Hibana: "Ouvrez des murs renforcés et préparez-vous à pénétrer rapidement.",
+    Twitch: "Détruisez les gadgets ennemis avant que votre équipe n'avance.",
+    Montagne: "Utilisez votre bouclier pour protéger vos coéquipiers tout en avançant.",
+    Glaz: "Profitez de la vue à longue distance pour repérer les ennemis derrière la fumée.",
+    Sledge: "Utilisez votre marteau pour créer des ouvertures surprises.",
+    Buck: "Utilisez votre fusil à pompe pour détruire des surfaces en hauteur.",
+    Finka: "Activez votre boost avant de vous engager dans des combats.",
+    Maverick: "Ouvrez des trous dans les murs pour créer des angles de tir.",
+    Kali: "Utilisez votre fusil à longue portée pour neutraliser les menaces à distance.",
+    
+    Bandit: "Utilisez vos électrocuteurs pour garder les murs renforcés.",
+    Mute: "Placez vos brouilleurs stratégiquement pour bloquer les drones.",
+    Doc: "Soignez vos coéquipiers dès que possible pour garder l'avantage.",
+    Echo: "Utilisez vos drones pour désorienter les attaquants.",
+    Kapkan: "Placez vos pièges près des entrées pour capturer les ennemis.",
+    Valkyrie: "Déployez vos caméras pour donner des informations vitales à votre équipe.",
+    Jäger: "Interceptez les projectiles ennemis avant qu'ils n'atteignent votre équipe.",
+    Lesion: "Placez vos mines dans des endroits inattendus pour ralentir les ennemis.",
+    Caveira: "Neutreisez les ennemis en silence pour éviter d'être détecté.",
+    Melusi: "Utilisez vos dispositifs pour ralentir les ennemis avant qu'ils n'attaquent.",
+    Warden: "Utilisez votre capacité pour contrer les attaques à la fumée.",
+    Thorn: "Placez vos pièges dans des zones stratégiques pour maximiser les éliminations."
+};
+
